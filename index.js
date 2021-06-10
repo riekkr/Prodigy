76ed01e0e175c3fce880c72b9edb808906a42401
ThaddeusKKR <thaddeuskkr@gmail.com>
2021-06-11 02:30:03 +0800

    Fixing undefined error in index

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ for (const file of admin) {
     client.commands.set(cmd.name.toLowerCase(), cmd);
 }
 for (const file of util) {
+    client.util = {};
     client.util[file.split('.')[0]] = require(`./util/${file}`);
 }
 logger.info(`${client.commands.size} commands and ${Object.keys(client._events).length} events loaded.`);
