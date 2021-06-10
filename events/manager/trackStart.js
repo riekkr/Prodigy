38095226cbc21e76337c9475493920533f1f2c91
ThaddeusKKR <thaddeuskkr@gmail.com>
2021-06-11 02:24:57 +0800

    Fix single queue nowplaying message error

--- events/manager/trackStart.js
+++ events/manager/trackStart.js
@@ -3,12 +3,23 @@ const prettyms = require('pretty-ms');
 
 module.exports = async (client, player, track) => {
     let channel = client.channels.cache.get(player.textChannel);
+    if (player.queue.length < 1) {
+        const embed = new MessageEmbed()
+            .setAuthor('Now playing')
+            .setDescription(`**[${track.title}](${track.uri})** \`${prettyms(track.duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${track.requester.tag}** (${track.requester.toString()})`)
+            .setFooter(`No tracks in queue | ${client.config.defaultFooter}`)
+            .setColor(client.config.defaultColor)
+            .setThumbnail(track.displayThumbnail('maxresdefault') || track.displayThumbnail('hqdefault') || track.displayThumbnail('mqdefault') || track.thumbnail);
+        let msg = await channel.send(embed);
+        if (channel.lastNowPlayingMessage) channel.lastNowPlayingMessage.delete();
+        channel.lastNowPlayingMessage = msg;
+    }
     if (!player.queue[0].uri) await player.queue[0].resolve();
 
     const embed = new MessageEmbed()
         .setAuthor('Now playing')
         .setDescription(`**[${track.title}](${track.uri})** \`${prettyms(track.duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${track.requester.tag}** (${track.requester.toString()})\n\n**Next in queue**\n[${player.queue[0].title}](${player.queue[0].uri}) \`${prettyms(player.queue[0].duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${player.queue[0].requester.tag}** (${player.queue[0].requester.toString()})`)
-        .setFooter(`${player.queue.size} tracks in queue | ${client.config.defaultFooter}`)
+        .setFooter(`${player.queue.size} track(s) in queue | ${client.config.defaultFooter}`)
         .setColor(client.config.defaultColor)
         .setThumbnail(track.displayThumbnail('maxresdefault') || track.displayThumbnail('hqdefault') || track.displayThumbnail('mqdefault') || track.thumbnail);
     let msg = await channel.send(embed);
