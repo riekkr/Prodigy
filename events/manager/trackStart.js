f5cf81eb1337201df0ca31b769853e34e5b1c36a
ThaddeusKKR <thaddeuskkr@gmail.com>
2021-06-11 18:09:24 +0800

    Add icon and profile picture URLs in trackStart event

--- events/manager/trackStart.js
+++ events/manager/trackStart.js
@@ -6,9 +6,9 @@ module.exports = async (client, player, track) => {
     let channel = client.channels.cache.get(player.textChannel);
     if (player.queue.length < 1) {
         const embed = new MessageEmbed()
-            .setAuthor('Now playing')
+            .setAuthor('Now playing', track.requester.avatarURL())
             .setDescription(`**[${track.title}](${track.uri})** \`${prettyms(track.duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${track.requester.tag}** (${track.requester.toString()})`)
-            .setFooter(`No tracks in queue | ${client.config.defaultFooter}`)
+            .setFooter(`No tracks in queue | ${client.config.defaultFooter}`, channel.guild.iconURL())
             .setColor(client.config.defaultColor)
             .setThumbnail(track.displayThumbnail('maxresdefault') || track.displayThumbnail('hqdefault') || track.displayThumbnail('mqdefault') || track.thumbnail);
         let msg = await channel.send(embed);
@@ -19,9 +19,9 @@ module.exports = async (client, player, track) => {
     if (!player.queue[0].uri) await player.queue[0].resolve();
 
     const embed = new MessageEmbed()
-        .setAuthor('Now playing')
+        .setAuthor('Now playing', track.requester.avatarURL())
         .setDescription(`**[${track.title}](${track.uri})** \`${prettyms(track.duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${track.requester.tag}** (${track.requester.toString()})\n\n**Next in queue**\n[${player.queue[0].title}](${player.queue[0].uri}) \`${prettyms(player.queue[0].duration, { colonNotation: true, secondsDecimalDigits: 0 })}\`\nRequested by: **${player.queue[0].requester.tag}** (${player.queue[0].requester.toString()})`)
-        .setFooter(`${player.queue.size} track(s) in queue | ${client.config.defaultFooter}`)
+        .setFooter(`${player.queue.size} track(s) in queue | ${client.config.defaultFooter}`, channel.guild.iconURL())
         .setColor(client.config.defaultColor)
         .setThumbnail(track.displayThumbnail('maxresdefault') || track.displayThumbnail('hqdefault') || track.displayThumbnail('mqdefault') || track.thumbnail);
     let msg = await channel.send(embed);
