bf71ea05a73980b335d4c588052f1e79fa2d46aa
thaddeuskkr <thaddeuskkr@gmail.com>
2021-06-26 13:07:43 +0800

    Added activity and config

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,8 @@
 module.exports = async (client) => {
     client.logger.info(`Logged in as ${client.user.tag} and serving ${client.guilds.cache.size} servers with ${client.users.cache.size} users`);
     await client.manager.init(client.user.id);
+    await client.user.setStatus(client.config.status);
+    await client.user.setActivity(client.config.activity, {
+        type: 'PLAYING' 
+    });
 };
