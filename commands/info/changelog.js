8ef0244809a7901749dddeae85679748d73d505d
Thaddeus Kuah <thaddeuskkr@gmail.com>
2021-08-12 00:33:29 +0800

    v3: Changelog command

---
+++ commands/info/changelog.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+module.exports = {
+    name: 'changelog', // Command name
+    description: 'Shows the changelog for the bot.', // Short description of what the command does
+    aliases: ['cl'], // An array of alternate commands that trigger the command
+    usage: '{p}changelog', // How the command is invocated, "{p}" is replaced by the prefix in the server
+    ownerOnly: false, // Restricts the command to the bot owner
+    requiredPermissions: [], // An array of permissions the user needs to run the command
+    dj: false, // Whether DJ only mode being on will prevent the command from being run
+    sameVC: false, // Requires the user to be in the same voice channel as the bot to run the command (Not done)
+
+    async execute(client, message) {
+        const changelog = fs.readFileSync('./changelog.txt', 'utf8');
+        message.reply(`\`\`\`diff\n${changelog}\n\`\`\``);
+    }
+};
