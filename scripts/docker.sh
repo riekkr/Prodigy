99f4e63ef1a1d4a1b3b6124ff9dbf931ac16148f
Thaddeus Kuah <thaddeuskkr@gmail.com>
2021-08-15 12:41:46 +0800

    Add fast forward only to docker update script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,7 +2,7 @@ echo "*****************************"
 echo "**  Prodigy Docker Update  **"
 echo "*****************************"
 echo ">> Updating Prodigy..."
-git pull origin master > update.log
+git pull origin master --ff-only > update.log
 echo ">> Building Docker container..."
 docker-compose build > update.log
 echo ">> Restarting Prodigy..."
